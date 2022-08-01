551f79bf8416f859ccb741ecc1e776a0119fa490
batyrbek01 <batyr070605@gmail.com>
2022-08-01 14:23:53 +0300

    dz1

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+ String nameOfStudents = "UsenovBatyr";
+
+ final int NUM = 25;
+
+String word = "tea";
+
+String tea ="apple";
+        System.out.println(nameOfStudents + NUM + word + tea);
+
+
+        int num = 5;
+
+      if (num < 0){
+          System.out.println("вы сщхранили отрицательное число!");
+        }else if (num >0){
+          System.out.println("вы сохранили положительное число!");
+      }else {
+          System.out.println("вы сохранили число 0");
+      }
+
+
+
+            }
+
+
+        }
+
